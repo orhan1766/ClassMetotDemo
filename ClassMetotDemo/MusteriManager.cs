fae65cfa78ac06374e89dfedb4254fdf51937966
orhan1766@gmail.com <orhan1766@gmail.com>
2021-01-20 16:04:55 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("{0} adlı müşteri eklendi.",musteri.Ad);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("{0} adlı müşteri silindi.", musteri.Ad);
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri
+            {
+                Id=1,
+                Ad="Orhan",
+                Sehir="Edirne",
+                YeniMusteri=true
+            };
+
+            Musteri musteri2 = new Musteri
+            {
+                Id = 2,
+                Ad = "Mesut",
+                Sehir = "Bursa",
+                YeniMusteri = false
+            };
+
+            Musteri musteri3 = new Musteri
+            {
+                Id = 3,
+                Ad = "Emre",
+                Sehir = "Sakarya",
+                YeniMusteri = false
+            };
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            Console.WriteLine("-------------");
+            musteriManager.Listele(musteriler);
+            Console.WriteLine("-------------");
+            musteriManager.Sil(musteri1);
+
+        }
+    }
+}
